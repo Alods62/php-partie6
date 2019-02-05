e279d69461aa07e71f9c41d504ec08e1eb87493a
Alods62 <42915139+Alods62@users.noreply.github.com>
2019-02-05 09:31:08 +0100

    Add files via upload

---
+++ exercice7/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Partie 6</title>
+
+</head>
+
+<body>
+    
+    <h1>Exercice 7</h1>
+    
+    <p><a href="bonjour.php?firstname=Aloïs&amp;lastname=De_Schepper">Lien</a></p>    
+    
+</body>
+</html>
